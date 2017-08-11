92834a8e4c3f8d7fd53f94f54532fe19cd598157
GaoHongchen <1656738573@qq.com>
2017-08-11 17:49:43 +0800

    formatting code

--- include/CalibCornerPatch.h
+++ include/CalibCornerPatch.h
@@ -11,33 +11,33 @@ using namespace TooN;
 class CalibCornerPatch
 {
 public:
-  struct Params
-  {
-    Params();
-    Matrix<2> m2Warp();
-    Vector<2> v2Pos;
-    Vector<2> v2Angles;
-    double dMean;
-    double dGain;
-  };
-  
-  CalibCornerPatch(int nSideSize = 8);
-  bool IterateOnImage(Params &params, CVD::Image<CVD::byte> &im);
-  bool IterateOnImageWithDrawing(Params &params, CVD::Image<CVD::byte> &im);
+    struct Params
+    {
+        Params();
+        Matrix<2> m2Warp();
+        Vector<2> v2Pos;
+        Vector<2> v2Angles;
+        double dMean;
+        double dGain;
+    };
 
- protected:
-  void MakeTemplateWithCurrentParams();
-  void FillTemplate(CVD::Image<float> &im, Params params);
-  double Iterate(CVD::Image<CVD::byte> &im);
-  Params mParams;
-  CVD::Image<float> mimTemplate;
-  CVD::Image<Vector<2> > mimGradients;
-  CVD::Image<Vector<2> > mimAngleJacs;
-  
-  void MakeSharedTemplate();
-  static CVD::Image<float> mimSharedSourceTemplate;
+    CalibCornerPatch(int nSideSize = 8);
+    bool IterateOnImage(Params &params, CVD::Image<CVD::byte> &im);
+    bool IterateOnImageWithDrawing(Params &params, CVD::Image<CVD::byte> &im);
 
-  double mdLastError;
+protected:
+    void MakeTemplateWithCurrentParams();
+    void FillTemplate(CVD::Image<float> &im, Params params);
+    double Iterate(CVD::Image<CVD::byte> &im);
+    Params mParams;
+    CVD::Image<float> mimTemplate;
+    CVD::Image<Vector<2> > mimGradients;
+    CVD::Image<Vector<2> > mimAngleJacs;
+
+    void MakeSharedTemplate();
+    static CVD::Image<float> mimSharedSourceTemplate;
+
+    double mdLastError;
 };
 
 
